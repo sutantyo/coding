c80ea4ad5bd06d88ce354d0b0c8817b0f66191f0
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2022-02-14 03:26:50 +1100

    routine

--- code/pq2.cpp
+++ code/pq2.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-	priority_queue<pair<int,pair<string,int>>> pq;
+	priority_queue<pair<int,pair<char,int>>> pq;
 
-	pq.push(make_pair(2,make_pair("a",6)));
-	pq.push(make_pair(2,make_pair("b",3)));
-	pq.push(make_pair(4,make_pair("a",3)));
-	pq.push(make_pair(2,make_pair("a",7)));
-	pq.push(make_pair(5,make_pair("a",3)));
+
+	pq.push({3,make_pair('c',1)});
+// alternatively: pq.emplace(3,make_pair('c',1));
+	pq.emplace(5,make_pair('a',2));
+	pq.push(make_pair(3,make_pair('a',4)));
+	pq.push(make_pair(5,make_pair('b',4)));
+	pq.push(make_pair(5,make_pair('b',3)));
+	pq.push(make_pair(5,make_pair('b',3)));
 
 	while(!pq.empty()){
 		cout << pq.top().first << " " << pq.top().second.first;
